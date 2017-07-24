1a6a5e964381e7a6f53ea73984530f01eaf39fd1
Matthijs Kuiper <info@matthijskuiper.nl>
2017-07-24 20:16:25 +0200

    Update Mixfile and license

--- mix.exs
+++ mix.exs
@@ -5,29 +5,29 @@ defmodule ExStyledoc.Mixfile do
     [app: :ex_styledoc,
      version: "0.1.0",
      elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: "Living style guide generation for your Elixir projects",
+     build_embedded: Mix.env == :dev,
+     start_permanent: Mix.env == :dev,
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+   [
+      maintainers: ["Matthijs Kuiper", "Marcel Horlings"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/DefactoSoftware/ex_styledoc",
+        "Docs" => "http://hexdocs.pm/ex_styledoc/"
+      }
+    ]
   end
 end
