0850348042773eafe7fb8cd56cc1039e82e40a60
Matthijs Kuiper <info@matthijskuiper.nl>
2017-07-25 12:48:11 +0200

    Only eval and render `example` code blocks

--- lib/ex_styledoc/parser.ex
+++ lib/ex_styledoc/parser.ex
@@ -4,24 +4,16 @@ defmodule ExStyledoc.Parser do
   """
 
   import Phoenix.HTML, only: [safe_to_string: 1]
-  # import Phoenix.HTML.Tag, only: [content_tag: 3]
 
   @doc """
   Parse a styledoc.
   """
   def parse_styledoc(doc) do
-    Regex.replace(~r/```(.*?)```/s, doc, &parse_example/2)
+    Regex.replace(~r/```example(.*?)```/s, doc, &parse_example/2)
     |> ExStyledoc.Markdown.to_html()
   end
 
   defp parse_example(_, string) do
-    # [
-    #   content_tag(:div, eval_example(string), class: "example"),
-    #   content_tag(:div, string, class: "code")
-    # ]
-    # |> Enum.map(fn(x) -> safe_to_string(x) end)
-    # |> Enum.join("\n")
-
     string = String.trim(string)
     example = eval_example(string) |> safe_to_string()
 
